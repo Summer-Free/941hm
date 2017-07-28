07448dbc865c01413b5c8ad16441b7baa5f3a9bb
Summber-Free <784443537@qq.com>
2017-07-28 15:06:29 +0800

    from jack 样式调整和页面功能优化

--- themes/ecmoban_dsc/js/js/free_trial.js
+++ themes/ecmoban_dsc/js/js/free_trial.js
@@ -3,28 +3,12 @@ $(function() {
 	//头部图片轮播
 	$(".slide-banner").slideFade();
 
-	//休闲餐桌/复古沙发/文艺书桌 切换
-	function tabjs(ulClass,contentClass) {
-
-		var $tab_ul = $(ulClass),
-			$tab_content = $(contentClass),
-			$tab_li = $tab_ul.find("li"),
-			$tab_item = $tab_content.find(".item");
-
-		$tab_li.on("click",function() {
-
-			var $this = $(this),
-				index = $this.index();
-
-				$this.addClass("active").siblings().removeClass("active")
-				.parents("h2").siblings().children().eq(index).
-				fadeIn(1000).siblings().fadeOut(1000);
-
-		});
-
+	//进入页面后，跳到指定位置
+	function anchorJump(id) {
+		location.href = location.href + id;
 	};
 
-	//调用  2017-07-25注释
-	// tabjs(".js-tab",".js-content");
+	//调用
+	anchorJump("#jumpHere");
 
 });
