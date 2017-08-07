ee5952433b089376d41647b73b2bd2b856e58117
Summber-Free <784443537@qq.com>
2017-08-07 10:46:50 +0800

    from jack 样式调整

--- themes/ecmoban_dsc/js/js/experience_hall.js
+++ themes/ecmoban_dsc/js/js/experience_hall.js
@@ -7,6 +7,25 @@ $(function() {
 	$(".section-2 .wrap").getHeight();
 	$(".section-2 .wrap").height($(".section-2 .wrap").height() - 30);
 
-	
-	
+	//假分页,点击 第二页时，对元素进行排序
+	$(".tab-nav>a").on("click",function() {
+		var $this = $(this),
+			index = $this.index();
+
+			if(index === 1) {
+				var li_arr = $(".section-2 .wrap ul").eq(index).find("li");
+				 
+				li_arr.sort(function (a, b) {
+					var sort1 = $(a).data('sort') * 1;
+					var sort2 = $(b).data('sort') * 1;
+
+					return sort1 - sort2;
+				});
+				 
+				li_arr.detach().appendTo($(".section-2 .wrap ul").eq(index));
+			};
+
+			$this.parent().siblings().children().eq(index).fadeIn().siblings().fadeOut();
+	});
+
 });
