28ac4e2a49760744e69f1dda04f887211589524f
Summber-Free <784443537@qq.com>
2017-08-17 18:05:36 +0800

    from jack 首页改版

--- themes/ecmoban_dsc/js/js/index.js
+++ themes/ecmoban_dsc/js/js/index.js
@@ -3,34 +3,12 @@ $(function() {
 	//top bar
 	$(".slide-top-bar").slideTop();
 
-	//每日秒杀,图片滚动
-	$(".slide-mrms").slideScroll({
-		speed: 1500
-	});
-
-	//新品上市(上),图片滚动
-	$(".New-arrival .wrap-slide.top").slideScroll({
-		speed: 2000
-	});
-
-	//新品上市(下),图片滚动
-	$(".New-arrival .wrap-slide.btm").slideScroll({
-		speed: 2000
-	});
-
-	//限时优惠(上),图片滚动
-	$(".Personal-tailor .wrap-slide.top").slideScroll({
-		speed: 2000
-	});
-
-	//限时优惠(下),,图片滚动
-	$(".Personal-tailor .wrap-slide.btm").slideScroll({
-		speed: 2000
-	});
+	//每日秒杀
+	$(".main-body").slideScroll();
 
 });
 
 window.onload = function() {
 	//banner slide
-	$(".slide-banner").slideFade();
+	$("#header .btm .JS-slide").slideFade();
 };
