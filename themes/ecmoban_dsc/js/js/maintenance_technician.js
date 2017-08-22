ed6b7864ef40f52bb5a77bd5746190bf762c8e8c
Summber-Free <784443537@qq.com>
2017-08-22 11:32:49 +0800

    from jack 样式调整

--- themes/ecmoban_dsc/js/js/maintenance_technician.js
+++ themes/ecmoban_dsc/js/js/maintenance_technician.js
@@ -3,4 +3,25 @@ $(function() {
 	//头部图片轮播
 	$(".section-slide").slideFade();	
 
+	//假分页
+	$(".tab-nav>a").on("click",function() {
+		var $this = $(this),
+			index = $this.index();
+
+			if(index === 1) {
+				var li_arr = $(".section-2 .content ul").eq(index).find("li");
+				 
+				li_arr.sort(function (a, b) {
+					var sort1 = $(a).data('sort') * 1;
+					var sort2 = $(b).data('sort') * 1;
+
+					return sort1 - sort2;
+				});
+				 
+				li_arr.detach().appendTo($(".section-2 .content ul").eq(index));
+			};
+
+			$this.addClass("active").siblings().removeClass("active").parent().siblings().children().eq(index).fadeIn().siblings().fadeOut();
+	});
+
 });
