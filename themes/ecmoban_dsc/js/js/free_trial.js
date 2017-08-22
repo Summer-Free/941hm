bdc86e3496b471fbab12bce12f294d828354d1a9
Summber-Free <784443537@qq.com>
2017-08-22 17:05:58 +0800

    from jack 样式调整

--- themes/ecmoban_dsc/js/js/free_trial.js
+++ themes/ecmoban_dsc/js/js/free_trial.js
@@ -3,12 +3,4 @@ $(function() {
 	//头部图片轮播
 	$(".slide-banner").slideFade();
 
-	//进入页面后，跳到指定位置
-	function anchorJump(href,id) {
-		location.href = href + id;
-	};
-
-	//调用
-	anchorJump("free_trial.php","#jumpHere");
-
 });
